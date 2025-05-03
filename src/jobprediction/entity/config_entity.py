6ad039f718763d924a932bd3d6c8be41f9912cc7
Aman-Agnihotri36 <agnihotriaman36@gmail.com>
2025-05-03 10:53:21 +0530

    yes

--- src/jobprediction/entity/config_entity.py
+++ src/jobprediction/entity/config_entity.py
@@ -37,9 +37,16 @@ class PredictionConfig:
 
 class PredictionConfig:
     def __init__(self):
-        self.data_path = "C:/Users/agnih/OneDrive/Documents/Desktop/ml/carrier_prediction/artifacts/ingestion/raw_data/roo_data.csv"
-        self.model_path = "C:/Users/agnih/OneDrive/Documents/Desktop/ml/carrier_prediction/artifacts/training/model/job_prediction_model.pkl"
-
+        base_dir = os.path.dirname(__file__)  # directory of current file
+        
+        # Use relative paths
+        self.data_path = os.path.join(base_dir, '..', 'artifacts', 'ingestion', 'raw_data', 'roo_data.csv')
+        self.model_path = os.path.join(base_dir, '..', 'artifacts', 'training', 'model', 'job_prediction_model.pkl')
+        
+        # Convert to absolute paths for safety
+        self.data_path = os.path.abspath(self.data_path)
+        self.model_path = os.path.abspath(self.model_path)
+        
 class ConfigurationManager:
     def PredictionManager(self):
         return PredictionConfig()
