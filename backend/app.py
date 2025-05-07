f93aff3a8de8905cf0d2f4ee5e13e06929f3f108
Aman-Agnihotri36 <agnihotriaman36@gmail.com>
2025-05-07 22:48:32 +0530

    job_finder

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,13 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-from jobprediction.components.prediction import PredictionPipeline
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.jobprediction.components.prediction import PredictionPipeline
+
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from src.jobfinderai.agent import run_agent
+from src.jobfinderai.fetch_job import fetch_jobs
 
 
 app = Flask(__name__)
@@ -23,6 +26,25 @@ def predict_career():
         return jsonify({"prediction": prediction_result})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+@app.route("/recommended-jobs", methods=["POST"])
+def recommended_jobs():
+    data = request.json
+    predicted_role = data.get("role")
+
+    if not predicted_role:
+        return jsonify({"error": "No role provided"}), 400
+
+    try:
+        job_results = jobs_recommended(predicted_role)
+        return jsonify({"recommended_jobs": job_results})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+def jobs_recommended(predicted_role: str):
+    job_results = run_agent(predicted_role)
+    print("\n📄 Recommended Jobs:\n", job_results)
+    return job_results
 
 if __name__ == "__main__":
     app.run(debug=True)
